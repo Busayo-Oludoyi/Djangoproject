8f18ea3a12aa917863f2d26a97c351cc898953e5
Busayo-Oludoyi <139758103+Busayo-Oludoyi@users.noreply.github.com>
2025-09-30 22:55:09 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a new pyhton program
+print("Day Zero")
+print("First day again")
+first_name = "Brandon"
+email = "wert@ser.com"
+Age = 33 
+gpa = 3.1
+print(first_name)
+print(f"Hello {first_name}")
+is_student = True
+Is_over_18 = True
+for_sale = False
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
+
+age = float(33)
+print (age)
